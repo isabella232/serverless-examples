d6bc16440728290b70976b34495465830ef698c5
Jorge Antunes <ask@stoikerty.com>
2018-11-08 13:24:48 +0000

    add simple react example as well

---
+++ lambda-simple-react/handler.js
@@ -0,0 +1,32 @@
+"use strict";
+const React = require("react");
+const renderToString = require("react-dom/server").renderToString;
+
+console.log("Loading function");
+
+/**
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
+ */
+exports.render = (event, context, callback) => {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+
+  callback(null, {
+    statusCode: "200",
+    body: renderToString(
+      React.createElement("span", { className: "test-element" }, "Test Element")
+    ),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+};
+
+// Testing locally:
+// exports.render('', '', console.log);
